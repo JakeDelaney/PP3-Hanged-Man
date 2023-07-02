61d694b007ad886e8b230d39e2a56c683e15a7a2
Jake Delaney <jakedelaney24@gmail.com>
2023-07-02 17:41:51 +0000

    Add introduction section and input for username

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def main():
+    print("Welcome to Hangman's Quest!")
+    USER = input(str("Please enter your name: "))
+    print(f"Welcome to the game {USER}!")
+main()
