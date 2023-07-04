216d722e72f7bd7999753c93c77e48851177e101
Jake Delaney <jakedelaney24@gmail.com>
2023-07-04 20:51:30 +0000

    Add function for reading .txt files, and update welcome screen function

--- run.py
+++ run.py
@@ -1,18 +1,27 @@
 from art import * #import all from the art module
-from words import medium_word_list
+from words import medium_word_list # import our wordlist from the words.py file
 import random
 
 
-def welcome_screen_art():
+def read_txt_file(text):
+    """This function open a text file, and iterates through each line of the file.
+       The content of this file are then printed out to the terminal.
+    """
+    with open(text, "r") as file: 
+        for line in file:
+            print(line, end="")
+        print()
+
+
+def welcome_screen():
     """This function displays an ascii welcome logo and art piece.
     The skull art is stored in a seperate text file and is called
     using the open function. A for loop is used to iterate over each line
     and print the contents into the run.py file.
     """
     tprint("Hangman's Quest!")
-    with open("ascii_skull.txt", "r") as file:
-        for line in file:
-            print(line, end="")
+    read_txt_file("ascii_skull.txt")
+
 
 def main_menu(choice, USER):
     """This function comprises the main menu. From here the user can select 
@@ -20,36 +29,30 @@ def main_menu(choice, USER):
     """
     while True:
         if choice == '1':
-            display_rules(USER)
+            tprint("\n\nGAME RULES")
+            print(f"ALRIGHT {USER}, LISTEN UP CAREFULLY...THE RULES ARE AS FOLLOWS:\n")
+            read_txt_file("game_rules.txt")
             break
         elif choice == '2':
-            #start_game()
+            get_random_word()
             break
         else:
             print('Please enter either "1" or "2"')
             choice = input("Enter key: ")
-
-def display_rules(USER):
-    """This function displays game rules and information when called.
-       The function also refers to the user by their name.
-    """
-    tprint("\n\nGAME RULES")
-    print(f"""  ALRIGHT {USER}, LISTEN UP CAREFULLY...THE RULES ARE AS FOLLOWS:
-        
-    1. The game begins with a hidden word, each letter within this word is represented by a dash.
-    2. Your objective is to guess the hidden word correctly by suggesting letters one at a time.
-    3. If you correctly guess a letter in the hidden, all instances of that letter are revealed.
-    4. If you guess an incorrect letter, you receive a strike, and one of the hanged man's body parts is revealed.
-    5. You have a total of 6 guesses, failing to reveal the word within 6 tries will result in a loss.
-""")
             
+
 def get_random_word():
+    """This function pulls a randomized word from the word list
+    and creates ann obscured variant of that word using dashes
+    Both the randomized word and its hidden equivalent are then return.
+    """
     random_word = random.choice(medium_word_list)
     hidden_word = "-" * len(random_word)
     return random_word, hidden_word
 
+
 def main():
-    welcome_screen_art()
+    welcome_screen()
     print("\n\nWelcome to Hangman's Quest!")
     USER = input(str("Please enter your name: ")).upper()
     print(f"\nWelcome to the game {USER}!")
@@ -59,11 +62,13 @@ def main():
     """)
     menu_choice = input("Enter key: ")
     main_menu(menu_choice, USER)
-    retrieved_word = get_random_word()
 
-#main()
-retrieved_random_word, retrieved_hidden_word = get_random_word()
-print(retrieved_hidden_word)
-print(retrieved_random_word)
+    retrieved_random_word, retrieved_hidden_word = get_random_word()
+    print (retrieved_hidden_word)
+    print (retrieved_random_word)
+
+main()
+
+
 
 
